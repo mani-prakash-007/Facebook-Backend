d8d7d0e44e6e48a16834fc380ec75772b46505c9
mani-prakash-007 <manip8072@gmail.com>
2024-07-19 22:55:31 +0530

    Added TimeStamps to schemas

--- models/postSchema.js
+++ models/postSchema.js
@@ -1,25 +1,30 @@
 //Post Schema
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const postSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    feed: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: [String],
+    },
+    dislikes: {
+      type: [String],
+    },
+    comments: {
+      type: [String],
+    },
   },
-  feed: {
-    type: String,
-    required: true,
-  },
-  likes: {
-    type: [String],
-  },
-  dislikes: {
-    type: [String],
-  },
-  comments: {
-    type: [String],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Post", postSchema);
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,25 +1,30 @@
 //User Schema
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
